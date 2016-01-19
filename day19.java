45a3327d364544c45e620704e7e08d662482c605
Fadhil Suhendi <fadhilfathurrahman@rocketmail.com>
2016-01-19 12:04:13 -0700

    added day19

---
+++ day19.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+
+//Write your code here
+//Write your code here
+class Calculator implements AdvancedArithmetic{
+    
+    Calculator(){
+        
+    }
+    public int divisorSum(int n){
+        int result = 0;
+        for(int i = 1;i <= n;i++){
+            if(n % i == 0){
+                result += i;
+            }
+        }
+        return result;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+      	AdvancedArithmetic myCalculator=new Calculator(); 
+        int sum=myCalculator.divisorSum(n);
+        System.out.println("I implemented: AdvancedArithmetic\n"+sum);
+    }
+}
