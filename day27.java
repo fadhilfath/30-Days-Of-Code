a1a841fad00d91d9d7b1ef5da17b661b1eed794a
Fadhil Suhendi <fadhilfathurrahman@rocketmail.com>
2016-01-27 12:05:02 -0700

    added day27

---
+++ day27.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        System.out.println("4 3");
+        System.out.println("2 3 -1 -2");
+        System.out.println("5 4");
+        System.out.println("0 0 -2 -1 2");
+        System.out.println("6 2");
+        System.out.println("-1 2 2 1 3 3");
+        System.out.println("7 3");
+        System.out.println("0 0 2 -1 3 2 1");
+        System.out.println("8 2");
+        System.out.println("-1 2 1 5 6 3 6 1");
+        
+    }
+}
